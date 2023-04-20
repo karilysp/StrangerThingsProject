9c9aca85f99b66ff92e17b1f39def01d53fbc856
Karilys <karilysp@gmail.com>
2023-04-19 20:40:54 -0400

    updated form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Register } from './';
 
-function App () {
-    return <h1>App</h1>
+
+function App() {
+    const [token, setToken] = useState('');
+
+    function tokenCheck() {
+        if (window.localStorage.getItem('token')) {
+            setToken(window.localStorage.getItem('token'));
+        }
+    }
+
+    useEffect(() => {
+        tokenCheck();
+    }, [])
+
+    console.log('stateful token', token)
+
+    return (
+        <div>
+            <Register setToken={setToken} />
+        </div>
+    )
 }
 
 export default App;
